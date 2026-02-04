cfe5cb4ea8f610e8eb47e0f019337f2570f7164d
Navod Niwarshana <123630951+navodniwarshana@users.noreply.github.com>
2026-02-05 00:25:38 +0530

    Delete Backend directory

--- Backend/src/main/java/com/StudentCrudOperation/demo/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StudentCrudOperation.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.StudentCrudOperation.demo.Entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
